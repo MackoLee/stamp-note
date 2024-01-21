884ae15015110b16d12ef6af16d7f726cdba94ec
이윤건(Macko) <sangok1993@naver.com>
2024-01-21 16:32:51 +0900

    Create index.vue

---
+++ pages/stamp-note/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="grid grid-cols-10 gap-1 min-h-[inherit]">
+    <div class="col-span-3">
+      <div class="m-3">
+        <SidebarView />
+      </div>
+    </div>
+    <div class="col-span-7 border-l-2">
+      <NuxtPageView />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
